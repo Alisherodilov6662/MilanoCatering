e40ce5077ed3a110850d32c3b6bbebd08283d586
Alisher <alisherodilov482@gmail.com>
2023-01-19 03:52:59 -0800

    category entity created

--- src/main/java/com/example/entity/category/CategoryEntity.java
+++ src/main/java/com/example/entity/category/CategoryEntity.java
@@ -4,6 +4,10 @@ import com.example.entity.AttachEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -17,8 +21,10 @@ public class CategoryEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "photo_id")
+    private Long photoId;
     @OneToOne
-    @JoinColumn(name = "photo_id")
+    @JoinColumn(name = "photo_id",insertable = false,updatable = false)
     private AttachEntity photo;
 
     @Column(name = "name_uz")
@@ -33,8 +39,15 @@ public class CategoryEntity {
     @Column(name = "description_ru")
     private String descriptionRu;
 
+    @CreationTimestamp
+    @CreatedDate
     @Column(name = "created_date")
     private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @UpdateTimestamp
+    @Column(name = "update_date")
+    private LocalDateTime updatedDate;
 
 
 
