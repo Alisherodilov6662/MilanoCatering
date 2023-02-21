39a83458d35edf19edc131f39240e50265332b57
Alisher <alisherodilov482@gmail.com>
2023-02-21 15:14:38 -0800

    Category finish!

--- src/main/java/com/example/service/CategoryService.java
+++ src/main/java/com/example/service/CategoryService.java
@@ -2,19 +2,20 @@ package com.example.service;
 
 import com.example.dto.category.superCategory.CategoryCreationDto;
 import com.example.dto.category.superCategory.CategoryGetDTO;
-import com.example.dto.category.superCategory.CategoryGetShortInfo;
+import com.example.dto.category.superCategory.CategoryGetForPublish;
 import com.example.entity.category.CategoryEntity;
 import com.example.enums.Status;
 import com.example.exception.CategoryAlreadyExistsException;
 import com.example.exception.CategoryNotFoundException;
 import com.example.repositiry.CategoryRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -23,85 +24,63 @@ import java.util.Optional;
  * Author: Alisher Odilov
  * Date: 19.01.2023
  */
-
+@AllArgsConstructor
 @Service
-@RequiredArgsConstructor
 public class CategoryService {
-
     private final CategoryRepository categoryRepository;
 
     public CategoryCreationDto create(CategoryCreationDto dto) {
         Optional<CategoryEntity> byNameUz = categoryRepository.findByNameUz(dto.getNameUz());
         Optional<CategoryEntity> byNameRu = categoryRepository.findByNameRu(dto.getNameRu());
-        if (byNameUz.isPresent() || byNameRu.isPresent()) {
-            throw new CategoryAlreadyExistsException("Category is already exists");
+        if (!byNameUz.isPresent() && !byNameRu.isPresent()) {
+            this.categoryRepository.save(toEntity(dto));
+            return dto;
+        } else {
+            throw new CategoryAlreadyExistsException("Category already exists");
         }
-        categoryRepository.save(toEntity(dto));
-        return dto;
     }
 
-    public Boolean update(CategoryCreationDto dto, Long id) {
+    public CategoryCreationDto update(CategoryCreationDto dto, Long id) {
         CategoryEntity entity = getEntityById(id);
         entity.setPhotoId(dto.getPhoto());
         entity.setNameUz(dto.getNameUz());
         entity.setNameRu(dto.getNameRu());
         entity.setDescriptionUz(dto.getDescriptionUz());
         entity.setDescriptionRu(dto.getDescriptionRu());
-        categoryRepository.save(entity);
-        return true;
+        this.categoryRepository.save(entity);
+        return dto;
     }
 
-    public Boolean deleteById(Long id) {
+    public String deleteById(Long id) {
         CategoryEntity entity = getEntityById(id);
         entity.setVisible(false);
         categoryRepository.save(entity);
-        return true;
+        return "successfully deleted !";
     }
 
     public Page<CategoryGetDTO> getList(Integer page, Integer size, String language) {
         Pageable pageable = PageRequest.of(page, size);
-        Page<CategoryEntity> pageObj = categoryRepository.findByStatusAndVisibleTrue(pageable,Status.PUBLISHED);
-        List<CategoryEntity> content = pageObj.getContent();
-        long totalElements = pageObj.getTotalElements();
-        List<CategoryGetDTO> dtoList = new LinkedList<>();
-        content.forEach(entity -> dtoList.add(getDTO(entity, language)));
-        return new PageImpl<>(dtoList, pageable, totalElements);
-
-    }
-
-    public CategoryGetDTO getById(Long id) {
-        CategoryEntity entity = getEntityById(id);
-        return getDtoAll(entity);
-    }
-
-    public CategoryCreationDto getByIdForUpdate(Long id){
-        CategoryEntity entity=getEntityById(id);
-        CategoryCreationDto dto=new CategoryCreationDto();
-        dto.setNameUz(entity.getNameUz());
-        dto.setNameRu(entity.getNameRu());
-        dto.setDescriptionRu(entity.getDescriptionRu());
-        dto.setDescriptionUz(entity.getDescriptionUz());
-        dto.setPhoto(entity.getPhotoId());
-     return dto;
-    }
-
-    public Page<CategoryGetShortInfo> getListAll(Integer page, Integer size) {
-        Pageable pageable = PageRequest.of(page, size);
         Page<CategoryEntity> pageObj = categoryRepository.findByVisibleTrue(pageable);
         List<CategoryEntity> content = pageObj.getContent();
         long totalElements = pageObj.getTotalElements();
-        List<CategoryGetShortInfo> dtoList = new LinkedList<>();
-        content.forEach(entity -> dtoList.add(getShortDTO(entity)));
-        return new PageImpl<>(dtoList, pageable, totalElements);
+        List<CategoryGetDTO> dtoList = new LinkedList();
+        content.forEach((entity) -> {
+            dtoList.add(getDTO(entity, language));
+        });
+        return new PageImpl(dtoList, pageable, totalElements);
     }
 
-    private CategoryGetShortInfo getShortDTO(CategoryEntity entity) {
-        CategoryGetShortInfo dto=new CategoryGetShortInfo();
-        dto.setId(entity.getId());
-        dto.setName_Uz(entity.getNameUz());
-        return dto;
+    public Page<CategoryGetDTO> getListAll(Integer page, Integer size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<CategoryEntity> pageObj = categoryRepository.findAll(pageable);
+        List<CategoryEntity> content = pageObj.getContent();
+        long totalElements = pageObj.getTotalElements();
+        List<CategoryGetDTO> dtoList = new LinkedList();
+        content.forEach((entity) -> {
+            dtoList.add(getDtoAll(entity));
+        });
+        return new PageImpl(dtoList, pageable, totalElements);
     }
-
 
     private CategoryEntity toEntity(CategoryCreationDto dto) {
         CategoryEntity entity = new CategoryEntity();
@@ -109,58 +88,95 @@ public class CategoryService {
         entity.setNameRu(dto.getNameRu());
         entity.setDescriptionUz(dto.getDescriptionUz());
         entity.setDescriptionRu(dto.getDescriptionRu());
+        entity.setStatus(Status.NOT_PUBLISHED);
         entity.setPhotoId(dto.getPhoto());
         entity.setVisible(true);
-        entity.setStatus(Status.NOT_PUBLISHED);
         return entity;
     }
 
     private CategoryEntity getEntityById(Long id) {
         Optional<CategoryEntity> optional = categoryRepository.findById(id);
-        return optional.orElseThrow(() -> {
+        if (optional.isEmpty()) {
             throw new CategoryNotFoundException(" Category not found ! ");
-        });
+        } else {
+            return optional.get();
+        }
     }
 
     private CategoryGetDTO getDTO(CategoryEntity entity, String language) {
         CategoryGetDTO dto = new CategoryGetDTO();
         dto.setPhotoId(entity.getPhotoId());
         if (language.equals("UZ")) {
+            dto.setId(entity.getId());
             dto.setNameUz(entity.getNameUz());
             dto.setDescriptionUz(entity.getDescriptionUz());
         }
 
         if (language.equals("RU")) {
+            dto.setId(entity.getId());
             dto.setNameRu(entity.getNameRu());
             dto.setDescriptionRu(entity.getDescriptionRu());
         }
+
         return dto;
     }
 
     private CategoryGetDTO getDtoAll(CategoryEntity entity) {
         CategoryGetDTO dto = new CategoryGetDTO();
-        dto.setId(entity.getId());
-        dto.setCreatedDate(entity.getCreatedDate());
         dto.setPhotoId(entity.getPhotoId());
         dto.setNameRu(entity.getNameRu());
         dto.setNameUz(entity.getNameUz());
         dto.setDescriptionUz(entity.getDescriptionUz());
         dto.setDescriptionRu(entity.getDescriptionRu());
-        dto.setStatus(entity.getStatus());
         return dto;
     }
 
     public String changeStatus(Long id) {
         CategoryEntity entity = getEntityById(id);
-        if (entity.getStatus().equals(Status.NOT_PUBLISHED)) {
-            entity.setStatus(Status.PUBLISHED);
-            categoryRepository.save(entity);
-            return "Status successfully changed to PUBLISHED";
+        entity.setVisible(true);
+        this.categoryRepository.save(entity);
+        return "Status successfully changed";
+    }
+
+    public List<CategoryGetForPublish> getCategoryNotPublish() {
+        List<CategoryEntity> list = categoryRepository.findByStatusNotPublished(Status.NOT_PUBLISHED.name(), Boolean.TRUE);
+        if (list.isEmpty()) {
+            return null;
+        } else {
+            List<CategoryGetForPublish> listDto = new ArrayList();
+            list.forEach((entity) -> {
+                listDto.add(toDTO(entity));
+            });
+            return listDto;
         }
-        if (entity.getStatus().equals(Status.PUBLISHED)) {
-            entity.setStatus(Status.NOT_PUBLISHED);
+    }
+
+    private CategoryGetForPublish toDTO(CategoryEntity entity) {
+        CategoryGetForPublish dto = new CategoryGetForPublish();
+        dto.setId(entity.getId());
+        dto.setName_Uz(entity.getNameUz());
+        dto.setDescription_Uz(entity.getDescriptionUz());
+        return dto;
+    }
+
+    public Integer getQuantity() {
+        List<CategoryEntity> entities = categoryRepository.findByStatusNotPublished(Status.NOT_PUBLISHED.name(), Boolean.TRUE);
+        if (entities.isEmpty()) {
+            return 0;
+        } else {
+            Integer result = entities.size();
+            return result;
         }
-        categoryRepository.save(entity);
-        return "Status successfully changed to NOT_PUBLISHED";
+    }
+
+    public CategoryCreationDto getByIdForUpdate(Long id) {
+        CategoryEntity entity = getEntityById(id);
+        CategoryCreationDto dto = new CategoryCreationDto();
+        dto.setNameUz(entity.getNameUz());
+        dto.setNameRu(entity.getNameRu());
+        dto.setDescriptionRu(entity.getDescriptionRu());
+        dto.setDescriptionUz(entity.getDescriptionUz());
+        dto.setPhoto(entity.getPhotoId());
+        return dto;
     }
 }
